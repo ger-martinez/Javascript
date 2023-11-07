ca9a5e0dcd102191032a3a262388187a9c323439
ger-martinez <germartinez1402@gmail.com>
2023-11-06 22:30:01 -0300

    3ra entrega

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function agregarOperacion(montoInicial, tasaInteres) {
 
 // Función para calcular el monto total
 function calcular(event) {
-  event.preventDefault(); // Prevenir la acción predeterminada del formulario
+  event.preventDefault();
 
   // Obtenemos los valores ingresados por el usuario
   let monto_inicial = parseFloat(document.getElementById('montoInicial'));
@@ -37,11 +37,10 @@ function calcular(event) {
     document.getElementById('registro').innerHTML = 'Por favor, ingrese valores válidos para el monto inicial y la tasa de interés.';
   }
 }
-
 document.addEventListener('DOMContentLoaded', function () {
   const form = document.getElementById('simuladorForm');
   form.addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevenir la acción predeterminada del formulario
+    event.preventDefault();
 
     const montoInicial = parseFloat(document.getElementById('montoInicial').value);
     const tasaInteres = parseFloat(document.getElementById('tasaInteres').value);
@@ -54,23 +53,11 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
-
-
+// Agregar event listener al botón "Borrar Datos Guardados"
+document.querySelector('.btn-deleteAll').addEventListener('click', function () {
+  BorrarResultados();
+});
 // Función para borrar datos guardados
 function BorrarResultados() {
-  registro.operaciones = [];
-  guardar();
-  mostrar();
-}
-
-// Función para guardar los datos en algún lugar (puedes implementarla según tus necesidades)
-function guardar() {
-  // Puedes implementar la lógica de guardado aquí, como en una base de datos o almacenamiento local.
-  // Por ejemplo, podrías usar localStorage para almacenar los datos en el navegador.
-}
-
-// Función para mostrar los datos guardados
-function mostrar() {
-  // Puedes implementar la lógica de mostrar los datos aquí.
-  // Por ejemplo, podrías actualizar una tabla o lista de operaciones en la interfaz.
+  document.getElementById('registro').innerHTML = "";
 }
